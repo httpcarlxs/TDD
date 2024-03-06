caf275e58bd07e0161a621029e2da4741de5ec75
Brenda <brenda.alves@ccc.ufcg.edu.br>
2024-03-05 21:15:07 -0300

    adiciona teste para createTask

---
+++ src/test/TestTask.java
@@ -0,0 +1,19 @@
+package src.test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class TestTask {
+
+    @Test
+    public void testCreateTask() {
+        Task task = new Task("task1", "desc task1", LocalDate.parse("2024-03-05"), Priority.HIGH);
+
+        assertEquals("task1", task.getTitle());
+        assertEquals("desc task1", task.getDescription());
+        assertEquals(LocalDate.parse("2024-03-05"), task.getDate());
+        assertEquals(Priority.HIGH, task.getPriority());
+    }
+}
